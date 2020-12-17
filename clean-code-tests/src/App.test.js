aca71cff98b23bcde860af9b8c8a1c479b3762a7
Hafeezul Kareem Shaik <hafeezul.shaik@ibhubs.co.in>
2020-12-17 20:25:56 +0530

    Fix jest eslint error

--- clean-code-tests/src/App.test.js
+++ clean-code-tests/src/App.test.js
@@ -3,9 +3,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 describe("App test cases", () => {
-  test("renders learn react link", () => {
-    render(<App />);
-    const linkElement = screen.getByText(/Click Me/i);
-    expect(linkElement).toBeInTheDocument();
-  });
+   test("renders learn react link", () => {
+      render(<App />);
+      const linkElement = screen.getByText(/Click Me/i);
+      expect(linkElement).toBeInTheDocument();
+   });
 });
