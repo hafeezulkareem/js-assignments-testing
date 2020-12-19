88fa73482aceeb26f9ff07bbe8768cb895118903
Hafeezul Kareem Shaik <hafeezul.shaik@ibhubs.co.in>
2020-12-19 12:28:29 +0530

    Add test for componentDidMount methods

--- react-life-cycle-methods-testing/src/App.test.js
+++ react-life-cycle-methods-testing/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { mount } from "enzyme";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from "./App";
+
+describe("Testing App", () => {
+   it("should test componentDidMount", () => {
+      const mockFetch = jest.fn();
+
+      const counterComponent = mount(<App fetch={mockFetch} />);
+      expect(mockFetch).toHaveBeenCalled();
+   });
 });
