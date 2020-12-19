eb763747cd449f17205021300feac5fca8642a3c
Hafeezul Kareem Shaik <hafeezul.shaik@ibhubs.co.in>
2020-12-19 12:41:04 +0530

    Add test for componentWillUnmount

--- react-life-cycle-methods-testing/src/App.js
+++ react-life-cycle-methods-testing/src/App.js
@@ -3,17 +3,39 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         count: 0,
+      };
+   }
+
    componentDidMount() {
       const { fetch } = this.props;
-      fetch();
+      if (fetch) {
+         fetch();
+      }
+      this.timer = setTimeout(this.autoIncrementCount, 1000);
    }
+
+   componentWillUnmount() {
+      if (this.timer) {
+         clearTimeout(this.timer);
+         this.timer = undefined;
+      }
+   }
+
+   autoIncrementCount = () => {
+      this.setState((state) => ({ count: state.count + 1 }));
+   };
 
    render() {
       return (
          <div className="App">
             <header className="App-header">
                <p>
-                  Edit <code>src/App.js</code> and save to reload.
+                  Edit <code>src/App.js</code> and save to reload.{" "}
+                  <span>{this.state.count}</span>
                </p>
                <a
                   className="App-link"
--- react-life-cycle-methods-testing/src/App.test.js
+++ react-life-cycle-methods-testing/src/App.test.js
@@ -7,7 +7,18 @@ describe("Testing App", () => {
    it("should test componentDidMount", () => {
       const mockFetch = jest.fn();
 
-      const counterComponent = mount(<App fetch={mockFetch} />);
+      mount(<App fetch={mockFetch} />);
       expect(mockFetch).toHaveBeenCalled();
+   });
+
+   it("should test componentWillUnmount", () => {
+      const mockFetch = jest.fn();
+
+      const component = mount(<App fetch={mockFetch} />);
+      const componentInstance = component.instance();
+
+      expect(componentInstance.timer).toBeDefined();
+      component.unmount();
+      expect(componentInstance.timer).toBeUndefined();
    });
 });
